6489d3d40c278edc27f0523b592503e30b9cd97e
Sam Herring <samherring111@gmail.com>
2021-05-15 12:51:08 +1000

    feat: Project Init

---
+++ templates/component/TemplateName.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './TemplateName.module.css';
+
+const TemplateName: React.FC = () => {
+  return(
+  <div className={styles.TemplateName}>
+    TemplateName Component
+  </div>
+  );
+};
+
+export default TemplateName;
